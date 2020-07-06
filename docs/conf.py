4e82325a51dc4178078d6181f4ec0b3a059138a8
Javier Vera <jverad@MacBook-Pro-de-administrador.local>
2020-07-05 23:27:24 -0400

    Add RST Extension Readme File

--- docs/conf.py
+++ docs/conf.py
@@ -176,7 +176,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ReadtheDocsTemplatedoc'
+htmlhelp_basename = 'renegadosdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -196,7 +196,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'ReadtheDocsTemplate.tex', u'Renegados Documentation',
+  ('index', 'renegados.tex', u'Renegados Documentation',
    u'Read the Docs', 'manual'),
 ]
 
@@ -226,7 +226,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'readthedocstemplate', u'Renegados Documentation',
+    ('index', 'renegados', u'Renegados Documentation',
      [u'Read the Docs'], 1)
 ]
 
@@ -240,8 +240,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'ReadtheDocsTemplate', u'Renegados Documentation',
-   u'Read the Docs', 'ReadtheDocsTemplate', 'One line description of project.',
+  ('index', 'renegados', u'Renegados Documentation',
+   u'Read the Docs', 'renegados', 'One line description of project.',
    'Miscellaneous'),
 ]
 
